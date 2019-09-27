c41c25619cefa010aab83c21f1aaee3d46b3443b
刘文义 <wenyi.lwy@120yibao.com>
2019-09-27 11:58:01 +0800

    init

--- scheduler_job.py
+++ scheduler_job.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(run_job, 'cron', hour=11, minute=22)
     logger = LoggerFactory("schedulers_job.class")
+
     try:
         scheduler.start()
         logger.info("定时将日志数据写入数据库成功")
